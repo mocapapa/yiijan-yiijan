f1618a871d998ffdae05b5c3e1fc49b8bfcd71a1
choco.moca.colon <choco.moca.colon@5580d20a-ebb9-11dd-9c15-fdd50ba39c1e>
2009-01-28 14:00:44 +0000

    カレンダーコンポーネントの完成

--- trunk/yiijan/protected/components/views/calendar.php
+++ trunk/yiijan/protected/components/views/calendar.php
@@ -3,9 +3,16 @@
 include_once('generate_calendar.php');
 $locale='ja_JP.utf8';
 $setlocale = setlocale(LC_ALL,  $locale);
+
 $today = date('j', time());
-$days = array($today=>array(NULL,NULL,'<span style="color:#990000; font-weight: bold;">'.$today.'</span>'),
-	      8=>array('/weblog/arch/2009/Jan/8', 'linked-day'));
+$days[$today] = array(NULL,NULL,'<span style="color:#990000; font-weight: bold;">'.$today.'</span>');
+
+$post = new Post;
+$days = array();
+foreach($post->findArtclePostedThisMonth() as $article):
+  $days[date('j', $article->createTime)]  = array('/demos/yiijan/poston/'.$article->createTime, 'linked-day');
+endforeach;
+
 echo generate_calendar(Date("Y"), Date("n"), $days, 3);
 ?>
 </center>
