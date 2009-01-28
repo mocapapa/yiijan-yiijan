68672d0d985d44064f09d421eade91e39381e997
choco.moca.colon <choco.moca.colon@5580d20a-ebb9-11dd-9c15-fdd50ba39c1e>
2009-01-28 09:09:24 +0000

    コンフィグ urlルーティングに日付でサーチする機能を追加

---
+++ yiijan/protected/config/main.php
@@ -0,0 +1,66 @@
+<?php
+
+// uncomment the following to define a path alias
+// Yii::setPathOfAlias('local','path/to/local-folder');
+//
+// This is the main Web application configuration. Any writable
+// CWebApplication properties can be configured here.
+return array(
+	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+	'name'=>'blog',
+	'defaultController'=>'post',
+
+	// preloading 'log' component
+	'preload'=>array('log'),
+
+	// autoloading model and component classes
+	'import'=>array(
+		'application.models.*',
+		'application.components.*',
+	),
+
+	// using 'classic' theme
+	'theme'=>'classic',
+
+	// application-level parameters that can be accessed
+	// using Yii::app()->params['paramName']
+	'params'=>require(dirname(__FILE__).'/params.php'),
+
+	// application components
+	'components'=>array(
+		'log'=>array(
+			'class'=>'CLogRouter',
+			'routes'=>array(
+				array(
+					'class'=>'CFileLogRoute',
+					'levels'=>'error, warning',
+				),
+			),
+		),
+		'user'=>array(
+			// enable cookie-based authentication
+			'allowAutoLogin'=>true,
+			// force 401 HTTP error if authentication needed
+			'loginUrl'=>null,
+		),
+		'db'=>array(
+			'connectionString'=>'sqlite:'.dirname(__FILE__).'/../data/blog.db',
+			/* uncomment the following to use MySQL as database
+			'connectionString'=>'mysql:host=localhost;dbname=blog',
+			'username'=>'xyz',
+			'password'=>'xxx',
+			*/
+		),
+		'urlManager'=>array(
+				'showScriptName'=>false,
+				'urlFormat'=>'path',
+				'rules'=>array(
+				       'posts'=>'post/list',
+				       'post/<id:\d+>'=>'post/show',
+				       'post/update/<id:\d+>'=>'post/update',
+				       'tag/<tag>'=>'post/list',
+				       'poston/<date>'=>'post/postedon',
+					       ),
+				    ),
+			    ),
+);
