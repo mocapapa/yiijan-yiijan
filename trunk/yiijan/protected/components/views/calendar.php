347556a223c591b13f0da795491879518c515f0f
choco.moca.colon <choco.moca.colon@5580d20a-ebb9-11dd-9c15-fdd50ba39c1e>
2009-01-28 22:05:11 +0000

    bugfix

--- trunk/yiijan/protected/components/views/calendar.php
+++ trunk/yiijan/protected/components/views/calendar.php
@@ -9,7 +9,7 @@ $days[$today] = array(NULL,NULL,'<span style="color:#990000; font-weight: bold;"
 
 $post = new Post;
 foreach($post->findArtclePostedThisMonth() as $article):
-  $days[date('j', $article->createTime)]  = array('/demos/yiijan/postedon/'.$article->createTime, 'linked-day');
+$days[date('j', $article->createTime)]  = array(CHtml::normalizeUrl('postedon/'.$article->createTime), 'linked-day');
 endforeach;
 
 echo generate_calendar(Date("Y"), Date("n"), $days, 3);
