e3d54fd84c483cc720bb87ad2c4978ddb52696fb
choco.moca.colon <choco.moca.colon@5580d20a-ebb9-11dd-9c15-fdd50ba39c1e>
2009-01-28 06:36:35 +0000

    本日を強調表示

--- trunk/yiijan/protected/components/views/calendar.php
+++ trunk/yiijan/protected/components/views/calendar.php
@@ -3,6 +3,8 @@
 include_once('generate_calendar.php');
 $locale='ja_JP.utf8';
 $setlocale = setlocale(LC_ALL,  $locale);
-echo generate_calendar(Date("Y"), Date("n"), NULL, 3);
+$today = date('j', time());
+$days = array($today=>array(NULL,NULL,'<span style="color:#990000; font-weight: bold;">'.$today.'</span>'));
+echo generate_calendar(Date("Y"), Date("n"), $days, 3);
 ?>
 </center>
