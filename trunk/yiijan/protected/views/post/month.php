03a5cd24f3afa825207ccf8af8eeb8738d6f604d
choco.moca.colon <choco.moca.colon@5580d20a-ebb9-11dd-9c15-fdd50ba39c1e>
2009-01-29 10:19:43 +0000

    インプリ途中

---
+++ trunk/yiijan/protected/views/post/month.php
@@ -0,0 +1,12 @@
+<?php if(!empty($_GET['time'])): ?>
+<h3>Posts Issued in "<?php echo CHtml::encode(date('F, Y',$_GET['time'])); ?>"</h3>
+<?php endif; ?>
+
+<?php foreach($posts as $post): ?>
+<?php $this->renderPartial('_post',array(
+	'post'=>$post,
+)); ?>
+<?php endforeach; ?>
+
+<br/>
+<?php $this->widget('CLinkPager',array('pages'=>$pages)); ?>
